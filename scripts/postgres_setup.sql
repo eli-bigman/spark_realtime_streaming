e1d2cd1696384fa71fbd660745163c1ca57c6a04
Richard Elinam Nutsugah <richard.nutsugah@amalitech.com>
2026-01-30 14:34:10 +0000

    add col for transformed json

--- scripts/postgres_setup.sql
+++ scripts/postgres_setup.sql
@@ -1,10 +1,15 @@
 -- Create the table for storing e-commerce events
+-- DROP TABLE IF EXISTS ecommerce_events;
 CREATE TABLE IF NOT EXISTS ecommerce_events (
     event_id VARCHAR(50) PRIMARY KEY,
     event_type VARCHAR(20),
     user_id VARCHAR(50),
     product_id VARCHAR(50),
     timestamp TIMESTAMP,
+    device VARCHAR(20),
+    browser VARCHAR(20),
+    location VARCHAR(10),
+    ingestion_timestamp TIMESTAMP,
     metadata JSONB
 );
 -- Index on timestamp for time-based queries
